9213a7fa50ec4783b8861ed6a1b0a0f04ffce44b
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-25 03:47:44 +0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,20 @@ function App() {
     const email = form.email.value;
     const user = { name, email };
     console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        form.reset();
+      });
   };
 
   return (
@@ -32,6 +46,13 @@ function App() {
         <br />
         <input type="submit" value="Add User" />
       </form>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id} : {user.name} : {user.email}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
