f90d1343d3b9aa49d15a483a2668b332ce3b1a7a
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-24 23:54:43 +0600

    user btn e click korle value paitsi clg

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,27 @@ function App() {
       .then((data) => setUsers(data));
   }, []);
 
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+  };
+
   return (
     <>
       <h1>Users management system</h1>
       <h3>Number of users: {users.length}</h3>
-    </> 
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="text" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
   );
 }
 
